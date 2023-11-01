d3a94868134c4c8ee53a23898e64b490432a8a19
AjengCereliaEvin <ajengcereliaevin@mail.ugm.ac.id>
2023-11-01 15:22:45 +0700

    first commit

---
+++ app/src/main/java/com/example/tugaspertemuan10/JenisFragment.kt
@@ -0,0 +1,78 @@
+package com.example.tugaspertemuan10
+
+import android.R
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.navigation.fragment.findNavController
+import com.example.tugaspertemuan10.databinding.FragmentJenisBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [JenisFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class JenisFragment : Fragment() {
+    private lateinit var binding: FragmentJenisBinding
+
+    private val jenis = arrayOf(
+        "First Class Ticket",
+        "Business Class Ticket",
+        "Economy Class Ticket"
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentJenisBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            val adapterJenis = ArrayAdapter(
+                requireActivity(),
+                R.layout.simple_spinner_item,jenis
+            )
+            adapterJenis.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            spinnerJenisTiket.adapter = adapterJenis
+
+            spinnerJenisTiket.onItemSelectedListener=
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        val selectedJenis = jenis[position]
+
+                        Log.d("Jenis Tiket", selectedJenis)
+                    }
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                    }
+                }
+
+            btnBuyJenis.setOnClickListener {
+                val selectedJenis = spinnerJenisTiket.selectedItem.toString()
+                if (selectedJenis.isNotBlank()) {
+                    findNavController().navigateUp()
+                }
+            }
+        }
+    }
+}
